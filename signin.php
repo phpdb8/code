45b6d2eec56315b9b9248fcdf4751aeb5510b287
phpdb8 <132033708+phpdb8@users.noreply.github.com>
2023-04-28 11:48:20 +0800

    Update signin.php

--- signin.php
+++ signin.php
@@ -5,8 +5,8 @@
 
 <body>
     <form action="check.php" method="post">
-    請輸入帳號:<input type="text" name="id"><br/>
-    請輸入密碼:<input type="password" name="pwd"><br/>
+    使用者名稱:<input type="text" name="id"><br/>
+    密碼:<input type="password" name="pwd"><br/>
     <input type="submit"><input type="reset">
 </body>
 </html>
