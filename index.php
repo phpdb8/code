783370219a567fb0e989599a34115b9250cf3a71
phpdb8 <132033708+phpdb8@users.noreply.github.com>
2023-04-28 11:39:50 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+
+<body>
+    <form action="check.php" method="post">
+    請輸入帳號:<input type="text" name="id"><br/>
+    請輸入密碼:<input type="password" name="pwd"><br/>
+    <input type="submit"><input type="reset">
+</body>
+</html>
