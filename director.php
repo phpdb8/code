0a986ec8e6b0b62842c314310af1a7fedabbc1c6
phpdb8 <132033708+phpdb8@users.noreply.github.com>
2023-04-30 15:33:16 +0800

    Update director.php

--- director.php
+++ director.php
@@ -1,54 +1,32 @@
 <?php
-session_start();
-if (!isset($_SESSION['user_id'])) {
-    header("Location: login.php");
-    exit();
-}
+    $director_id = $_GET['director_id'];
+    $db = new mysqli('localhost', 'username', 'password', 'database_name');
+    if ($db->connect_error) {
+        die("Connection failed: " . $db->connect_error);
+    }
 
-$director_id = $_GET['id'];
-$sql = "SELECT * FROM directors WHERE id = $director_id";
-$result = mysqli_query($conn, $sql);
-$row = mysqli_fetch_assoc($result);
+    // 取得導演的資訊
+    $director_query = "SELECT * FROM directors WHERE id = $director_id";
+    $director_result = $db->query($director_query);
+    $director_row = $director_result->fetch_assoc();
 
-$sql_movies = "SELECT movies.id, movies.title, movies.poster, movies.release_date FROM movies 
-               JOIN movie_director ON movies.id = movie_director.movie_id 
-               JOIN directors ON movie_director.director_id = directors.id 
-               WHERE directors.id = $director_id";
-$result_movies = mysqli_query($conn, $sql_movies);
+    // 取得導演指導過的作品
+    $movies_query = "SELECT * FROM movies WHERE director_id = $director_id";
+    $movies_result = $db->query($movies_query);
 ?>
-
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <title><?php echo $row['name']; ?></title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <header>
-        <nav>
-            <ul>
-                <li><a href="index.php">首頁</a></li>
-                <li><a href="logout.php">登出</a></li>
-            </ul>
-        </nav>
-    </header>
-    <main>
-        <h1><?php echo $row['name']; ?></h1>
-        <div class="director-info">
-            <img src="<?php echo $row['photo']; ?>" alt="<?php echo $row['name']; ?>的照片">
-            <p><?php echo $row['bio']; ?></p>
-        </div>
-        <h2>執導作品</h2>
-        <div class="movie-grid">
-            <?php while ($row_movies = mysqli_fetch_assoc($result_movies)) { ?>
-                <div class="movie">
-                    <a href="movie.php?id=<?php echo $row_movies['id']; ?>"><img src="<?php echo $row_movies['poster']; ?>" alt="<?php echo $row_movies['title']; ?>"></a>
-                    <h3><?php echo $row_movies['title']; ?></h3>
-                    <p><?php echo $row_movies['release_date']; ?></p>
-                </div>
-            <?php } ?>
-        </div>
-    </main>
-</body>
+    <head>
+        <title><?php echo $director_row['name']; ?> 的導演頁面</title>
+    </head>
+    <body>
+        <h1><?php echo $director_row['name']; ?></h1>
+        <p>年齡：<?php echo $director_row['age']; ?></p>
+        <p>指導過的作品：</p>
+        <ul>
+            <?php while ($movies_row = $movies_result->fetch_assoc()): ?>
+                <li><a href="movie.php?movie_id=<?php echo $movies_row['id']; ?>"><?php echo $movies_row['title']; ?></a></li>
+            <?php endwhile; ?>
+        </ul>
+    </body>
 </html>
