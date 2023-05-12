0d997686b019504c7dc999c20f1a8e92477dba55
phpdb8 <132033708+phpdb8@users.noreply.github.com>
2023-05-12 10:39:35 +0800

    Update fail.php

--- fail.php
+++ fail.php
@@ -1,1 +1,27 @@
+<?php ob_start(); ?>
+<?php
+session_start();
 
+if($_SESSION["login"]=="No"){
+
+}else{
+    header("Location:error.php");
+}
+?>
+
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+
+<body>
+登入失敗!<br/>
+網頁將在三秒後跳轉至登入頁面或
+<a href="signin.php">點選這裡</a>
+<?php
+header("Refresh:3;url=signin.php")
+?>
+</body>
+
+</html>
+<?php ob_flush(); ?>
