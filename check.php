e5748367125fad297b5857ec677efe0eca00b5b2
phpdb8 <132033708+phpdb8@users.noreply.github.com>
2023-05-12 10:49:25 +0800

    Update check.php

--- check.php
+++ check.php
@@ -1,7 +1,11 @@
-$servername = "";
-$username = "username";
-$password = "password";
-$dbname = "moviesdb";
+    session_start();
+    $servername = "127.0.0.1:3306";
+    $username = "username";
+    $password = "password";
+    $dbname = "moviesdb";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
 
 <?php
 if(){
