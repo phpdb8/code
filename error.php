7ff2fc0b6fc0d70cbaa3435ea19a87ce2208ce6a
phpdb8 <132033708+phpdb8@users.noreply.github.com>
2023-05-12 10:41:12 +0800

    Update error.php

--- error.php
+++ error.php
@@ -1,1 +1,15 @@
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
 
+<body>
+非法進入網頁!<br/>
+網頁將在三秒後跳轉至登入頁面或
+<a href="signin.php">點選這裡</a>
+<?php
+header("Refresh:3;url=signin.php")
+?>
+</body>
+
+</html>
